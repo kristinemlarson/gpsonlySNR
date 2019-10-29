7298fe3551e77fe19b68028cbebfce0576beb6cb
Yuki Nakashima <nakashima0124@frontier.hokudai.ac.jp>
2019-10-29 18:40:53 +0900

    WIP update read_header_20obs_v3.f to read RINEX3

--- read_header_20obs_v3.f
+++ read_header_20obs_v3.f
@@ -10,7 +10,7 @@ c     allows 20 observables
       character*80 line, outline, dynfmt, dynfmt2
       logical  endofheader
       integer nobs,iobs(maxsat), iymd(3), ios
-      character*2 key(maxsat)
+      character*3 key(maxsat)
       character*4 station
       real*8 xrec, yrec, zrec
 c     returns receiver coordinates
@@ -28,48 +28,44 @@ c     returned to main code
       do while (.not.endofheader)
 c     KL 18mar05, fixed bug on nobs
         read (fileID,'(a80)') line
-        if (line(61:80).eq.'# / TYPES OF OBSERV') then
-          read(line, fmt='(I6)') nobs
-c         exit if more than 20 observables
-          if (nobs.gt.20) then
-             print*,'this code only supports <=20 observ types'
-             call exit
-          endif
+        if (line(1:1)=='G'.and.line(61:80).eq.'SYS / # / OBS TYPES')then
+!     GPS only
+          read(line(4:6), fmt='(I6)') nobs
 c   KL 19jan09 allowing more lines of OBS types
 c         first line has up to 9 OBS types
-          if (nobs .lt. 10) then
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", nobs, "(4X,A2))"
+          if (nobs .lt. 14) then
+            write(dynfmt, fmt='(A, 2X, I3.3)')
+     +                      "(6X,", nobs, "(1X,A3))"
             read(line, fmt=dynfmt) (key(i), i=1,nobs)
-c         between 10-18 OBS types
-          elseif (nobs.gt.9.and.nobs.le.18) then
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", 9, "(4X,A2))"
-            read(line, fmt=dynfmt) (key(i), i=1,9)
+c         between 14-26 OBS types
+          elseif (nobs.ge.14.and.nobs.le.26) then
+            write(dynfmt, fmt='(A, 2X, I3.3)')
+     +                      "(6X,", 13, "(1X,A3))"
+            read(line, fmt=dynfmt) (key(i), i=1,13)
 c           read the next line
             read (fileID,'(a80)') line
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", nobs-9, "(4X,A2))"
-            read(line, fmt=dynfmt) (key(i), i=10,nobs)
-c           this is more than 18 OBS types
+            write(dynfmt, fmt='(A, 2X, I3.3)')
+     +                      "(6X,", nobs-13, "(1X,A3))"
+            read(line, fmt=dynfmt) (key(i), i=13,nobs)
+c           this is more than 26 OBS types
           else
 c           first line
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", 9, "(4X,A2))"
-            read(line, fmt=dynfmt) (key(i), i=1,9)
+            write(dynfmt, fmt='(A, 2X, I3.3)')
+     +                      "(6X,", 13, "(1X,A3))"
+            read(line, fmt=dynfmt) (key(i), i=1,13)
 
 c           read the next line
             read (fileID,'(a80)') line
 c           reassign this second line
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", 9, "(4X,A2))"
-            read(line, fmt=dynfmt) (key(i), i=10,18)
+            write(dynfmt, fmt='(A, 1X, I3.3)')
+     +                      "(6X,", 13, "(1X,A3))"
+            read(line, fmt=dynfmt) (key(i), i=14,26)
 
 c           read the third line
             read (fileID,'(a80)') line
-            write(dynfmt, fmt='(A, I3.3, A)')
-     +                      "(6X,", nobs-18, "(4X,A2))"
-            read(line, fmt=dynfmt) (key(i), i=19,nobs)
+            write(dynfmt, fmt='(A, 1X, I3.3)')
+     +                      "(6X,", nobs-26, "(1X,A3))"
+            read(line, fmt=dynfmt) (key(i), i=27,nobs)
 
           endif
           print*, 'NUMBER OF OBSERVABLES ', nobs
@@ -97,16 +93,16 @@ c           read the third line
           iobs(i) = 0
       enddo
       do i = 1, nobs
-          if (key(i).eq.'l1' .or. key(i).eq.'L1') iobs(1) = i
-          if (key(i).eq.'l2' .or. key(i).eq.'L2') iobs(2) = i
-          if (key(i).eq.'c1' .or. key(i).eq.'C1') iobs(3) = i
-          if (key(i).eq.'p1' .or. key(i).eq.'P1') iobs(4) = i
-          if (key(i).eq.'p2' .or. key(i).eq.'P2') iobs(5) = i
-          if (key(i).eq.'s1' .or. key(i).eq.'S1') iobs(6) = i
-          if (key(i).eq.'s2' .or. key(i).eq.'S2') iobs(7) = i
-          if (key(i).eq.'s5' .or. key(i).eq.'S5') iobs(8) = i
-          if (key(i).eq.'s6' .or. key(i).eq.'S6') iobs(9) = i
-          if (key(i).eq.'s7' .or. key(i).eq.'S7') iobs(10) = i
-          if (key(i).eq.'s8' .or. key(i).eq.'S8') iobs(11) = i
+          if (key(i).eq.'l1c' .or. key(i).eq.'L1C') iobs(1) = i
+          if (key(i).eq.'l2p' .or. key(i).eq.'L2P') iobs(2) = i
+          if (key(i).eq.'c1c' .or. key(i).eq.'C1C') iobs(3) = i
+          if (key(i).eq.'c1p' .or. key(i).eq.'C1P') iobs(4) = i
+          if (key(i).eq.'c2p' .or. key(i).eq.'C2P') iobs(5) = i
+          if (key(i).eq.'s1c' .or. key(i).eq.'S1C') iobs(6) = i
+          if (key(i).eq.'s2p' .or. key(i).eq.'S2P') iobs(7) = i
+          if (key(i).eq.'s5i' .or. key(i).eq.'S5I') iobs(8) = i
+!          if (key(i).eq.'s6' .or. key(i).eq.'S6') iobs(9) = i
+!          if (key(i).eq.'s7' .or. key(i).eq.'S7') iobs(10) = i
+!          if (key(i).eq.'s8' .or. key(i).eq.'S8') iobs(11) = i
       enddo
       end
